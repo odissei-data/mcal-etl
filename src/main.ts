fcd5bdbf72d47fc25c79526f364f41eb4117528e
Philippe Renzen <philippe.renzen@triply.cc>
2023-05-01 15:53:31 +0200

    Updated to Triply ETL v2.0.0 (ESM update) #145
    TODO: update readme + CI/CD

--- src/main.ts
+++ src/main.ts
@@ -1,57 +1,63 @@
-import { a, addIri, environments, fromXlsx, iri, logRecord, pairs, Ratt as Etl, toTriplyDb } from '@triplydb/ratt'
-import { bibo, dct } from '@triplydb/ratt/lib/vocab'
+import {
+  Etl,
+  Source,
+  declarePrefix,
+  environments,
+  fromXlsx,
+  toTriplyDb,
+} from "@triplyetl/etl/generic";
+import { addIri, iri, pairs } from "@triplyetl/etl/ratt";
+import { logRecord } from "@triplyetl/etl/debug";
+import { bibo, dct, a } from "@triplyetl/etl/vocab";
 
 // Declare prefixes.
-const prefix_base = Etl.prefixer('https://example.org/')
-const prefix_id = Etl.prefixer(prefix_base('id/'))
+const prefix_base = declarePrefix("https://example.org/");
+const prefix_id = declarePrefix(prefix_base("id/"));
 const prefix = {
   id: prefix_id,
-  graph: Etl.prefixer(prefix_id('graph/')),
-}
+  graph: declarePrefix(prefix_id("graph/")),
+};
 
 const graph = {
-  instances: prefix.graph('instances'),
-}
+  instances: prefix.graph("instances"),
+};
 
 const destination = {
-  account:
-    Etl.environment === environments.Development
-      ? undefined
-      : 'odissei',
+  account: Etl.environment === environments.Development ? undefined : "odissei",
   dataset:
     Etl.environment === environments.Acceptance
-      ? 'mcal-acceptance'
+      ? "mcal-acceptance"
       : Etl.environment === environments.Testing
-        ? 'mcal-testing'
-        : 'mcal',
-}
+      ? "mcal-testing"
+      : "mcal",
+};
 
 export default async function (): Promise<Etl> {
-  const etl = new Etl({ defaultGraph: graph.instances })
+  const etl = new Etl({ defaultGraph: graph.instances });
   etl.use(
-    fromXlsx(Etl.Source.TriplyDb.asset('odissei', 'mcal', {
-      name: '20220610_MCAL_Inventory_ContentAnalysis.xlsx',
-    })),
+    fromXlsx(
+      Source.TriplyDb.asset("odissei", "mcal", {
+        name: "20220610_MCAL_Inventory_ContentAnalysis.xlsx",
+      })
+    ),
 
     addIri({
       prefix: prefix.id,
-      content: 'JournalID',
-      key: '_journal',
+      content: "JournalID",
+      key: "_journal",
     }),
-    pairs(iri(prefix.id, 'ArticleID'),
+    pairs(
+      iri(prefix.id, "ArticleID"),
       [a, bibo.AcademicArticle],
-      [dct.title, 'Title Article'],
-      [dct.isPartOf, '_journal'],
+      [dct.title, "Title Article"],
+      [dct.isPartOf, "_journal"]
     ),
-    pairs('_journal',
-      [a, bibo.Journal],
-      [dct.title, 'Journal'],
-    ),
+    pairs("_journal", [a, bibo.Journal], [dct.title, "Journal"]),
 
     logRecord(),
 
     //validateShacl(Etl.Source.file('static/model.trig')),
-    toTriplyDb(destination),
-  )
-  return etl
+    toTriplyDb(destination)
+  );
+  return etl;
 }
