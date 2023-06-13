d98ea3b2250cdc42beb2e367f257262a990b99e5
Wouter Beek <wouter@triply.cc>
2023-06-13 15:27:08 +0200

    ADDED: Changes during a working session.

--- src/main.ts
+++ src/main.ts
@@ -1,63 +1,67 @@
-import {
-  Etl,
-  Source,
-  declarePrefix,
-  environments,
-  fromXlsx,
-  toTriplyDb,
-} from "@triplyetl/etl/generic";
-import { addIri, iri, pairs } from "@triplyetl/etl/ratt";
-import { logRecord } from "@triplyetl/etl/debug";
-import { bibo, dct, a } from "@triplyetl/etl/vocab";
+import { Etl, Source, declarePrefix, environments, fromXlsx, toTriplyDb } from '@triplyetl/etl/generic'
+import { addIri, iri, pairs } from '@triplyetl/etl/ratt'
+import { logRecord } from '@triplyetl/etl/debug'
+import { bibo, dct, a } from '@triplyetl/etl/vocab'
+import { validate } from '@triplyetl/etl/shacl'
 
 // Declare prefixes.
-const prefix_base = declarePrefix("https://example.org/");
-const prefix_id = declarePrefix(prefix_base("id/"));
+const prefix_base = declarePrefix('https://mcal.odissei.nl/')
 const prefix = {
-  id: prefix_id,
-  graph: declarePrefix(prefix_id("graph/")),
-};
+  data: declarePrefix(prefix_base('data/')),
+  graph: declarePrefix(prefix_base('graph/')),
+  schema: declarePrefix(prefix_base('schema/'))
+}
+
+const schema = {
+  /**
+   * Category: property
+   *
+   * Label: has material
+   *
+   * Description: Type of material
+   */
+  material: prefix.schema('material')
+}
 
 const graph = {
-  instances: prefix.graph("instances"),
-};
+  instances: prefix.graph('instances'),
+  metadata: prefix.graph('metadata'),
+  schema: prefix.graph('schema')
+}
 
 const destination = {
-  account: Etl.environment === environments.Development ? undefined : "odissei",
+  account: Etl.environment === environments.Development ? undefined : 'odissei',
   dataset:
     Etl.environment === environments.Acceptance
-      ? "mcal-acceptance"
+      ? 'mcal-acceptance'
       : Etl.environment === environments.Testing
-      ? "mcal-testing"
-      : "mcal",
-};
+        ? 'mcal-testing'
+        : 'mcal'
+}
 
 export default async function (): Promise<Etl> {
-  const etl = new Etl({ defaultGraph: graph.instances });
+  const etl = new Etl({ defaultGraph: graph.instances })
   etl.use(
     fromXlsx(
-      Source.TriplyDb.asset("odissei", "mcal", {
-        name: "20220610_MCAL_Inventory_ContentAnalysis.xlsx",
-      })
+      Source.TriplyDb.asset('odissei', 'mcal',
+        { name: '20230613_MCAL_Inventory_ContentAnalysis.xlsx' })
     ),
-
     addIri({
-      prefix: prefix.id,
-      content: "JournalID",
-      key: "_journal",
+      prefix: prefix.data,
+      content: 'journalID',
+      key: '_journal'
     }),
-    pairs(
-      iri(prefix.id, "ArticleID"),
+    pairs(iri(prefix.data, 'articleID'),
       [a, bibo.AcademicArticle],
-      [dct.title, "Title Article"],
-      [dct.isPartOf, "_journal"]
+      [dct.title, 'title'],
+      [dct.isPartOf, '_journal']
     ),
-    pairs("_journal", [a, bibo.Journal], [dct.title, "Journal"]),
-
-    logRecord(),
-
-    //validateShacl(Etl.Source.file('static/model.trig')),
+    pairs('_journal',
+      [a, bibo.Journal],
+      [dct.title, 'journal']
+    ),
+    validate(Source.file('static/model.trig')),
     toTriplyDb(destination)
-  );
-  return etl;
+  )
+  return etl
 }
