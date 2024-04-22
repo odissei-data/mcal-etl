a457d8e31fd238923f4f676e4d970fa5b8970d27
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@vu.nl>
2024-04-22 15:13:42 +0200

    Added proper logging and extracting skos enrichments from dataverse API

--- dv.py
+++ dv.py
@@ -6,21 +6,28 @@
 
 import requests
 import json
+import logging
 
 from cache_to_disk import cache_to_disk, delete_disk_caches_for_function
 
 PORTAL='https://portal.odissei.nl'
 outputfile='cbs.csv'
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(encoding='utf-8', level=logging.INFO)
+
 @cache_to_disk(3)
 def get_datasets():
-  dv_list = requests.get(f'{PORTAL}/api/v1/dataverses/cbs/contents')
+  uri = f'{PORTAL}/api/v1/dataverses/cbs/contents'
+  logger.debug(f'Requesting datasets overview from {uri}')
+  dv_list = requests.get(uri)
   return json.loads(dv_list.content)
 
 @cache_to_disk(3)
 def get_dataset(doi):
-  url = f'{PORTAL}/api/v1/datasets/export?exporter=dataverse_json&persistentId={doi}'
-  ds_record = requests.get(url)
+  uri = f'{PORTAL}/api/v1/datasets/export?exporter=dataverse_json&persistentId={doi}'
+  logger.debug(f'Requesting dataset metadata from {uri}')
+  ds_record = requests.get(uri)
   return json.loads(ds_record.content)
 
 if __name__ == '__main__':
@@ -28,19 +35,33 @@ if __name__ == '__main__':
   # delete_disk_caches_for_function('get_dataset')
   with open(outputfile, 'w', encoding="utf-8") as f:
     dv_list = get_datasets()
-    f.write('DOI, alternativeTitle, publicationDate\n')
+    f.write('DOI, alternativeTitle, publicationDate, relatedSkosConcepts\n')
     for r in dv_list['data']:
       doi = r['persistentUrl']
       publicationDate = r['publicationDate']
       metadata = get_dataset(doi) 
+      concepts = ""
+      try:
+        for complexValue in metadata['datasetVersion']['metadataBlocks']['enrichments']['fields'][0]['value']:
+          for field in complexValue.values():
+            if field['typeName'] == 'vocabVarUri1':
+              if concepts:
+                concepts = concepts + f' {field['value']}'
+              else:
+                concepts = field['value']
+      except AttributeError:
+        logger.info(f'No SKOS vocabulary enrichments for {doi}')
+      except KeyError:
+        logger.info(f'No enrichments for {doi}')
+
       try: 
         for field in metadata['datasetVersion']['metadataBlocks']['citation']['fields']:
           if field['typeName'] == 'alternativeTitle':
             altTitle = field['value'][0]
             altTitle = altTitle.translate({91:95, 93:95})
-            resultString = f'{doi}, "{altTitle}", "{publicationDate}"\n'
+            resultString = f'{doi}, "{altTitle}", "{publicationDate}", "{concepts}"\n'
             f.write(resultString)
-            print(resultString) # debug only
+            logger.debug(resultString)
       except KeyError:
         print(f"Oops {metadata}")
   print(f"Results written to {outputfile}")
