6ade60f77e13ac3803b6bfea8edad4b65a3762de
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@vu.nl>
2024-04-30 14:22:28 +0200

    Minor: added docstrings to document key functions

--- dv.py
+++ dv.py
@@ -26,6 +26,8 @@ logger = logging.getLogger(__name__)
 
 @cache_to_disk(3)
 def get_datasets():
+  """Get an overview of all dataset ids by calling the /contents API.
+  Returns a Python dict version of the JSON returned by Dataverse."""
   uri = f'{PORTAL}/api/v1/dataverses/cbs/contents'
   logger.debug(f'Requesting datasets overview from {uri}')
   dv_list = requests.get(uri)
@@ -33,12 +35,16 @@ def get_datasets():
 
 @cache_to_disk(3)
 def get_dataset(doi):
+  """Get an overview of all metadata for the dataset identified by doi.
+    Returns a Python dict version of the JSON returned by Dataverse.
+  """
   uri = f'{PORTAL}/api/v1/datasets/export?exporter=dataverse_json&persistentId={doi}'
   logger.debug(f'Requesting dataset metadata from {uri}')
   ds_record = requests.get(uri)
   return json.loads(ds_record.content)
 
 def dataverse2csv():
+  """Loop over all datasets and write selected metadata to CSV."""
   with open(outputfile, 'w', encoding="utf-8") as f:
     dv_list = get_datasets()
     f.write('alternativeTitle, publicationDate, DOI, validFrom, validTill, relatedSkosConcepts\n')
